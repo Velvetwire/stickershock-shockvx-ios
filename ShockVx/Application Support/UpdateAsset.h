68e1017848c47fb7bc75a8f855607788395b1698
ebodnar <eric@velvetwire.com>
2020-10-11 16:15:25 -0700

    Missing from xcode commit

---
+++ ShockVx/Application Support/UpdateAsset.h
@@ -0,0 +1,17 @@
+//
+//  UpdateAsset.h
+//  ShockVx
+//
+//  Created by Eric Bodnar on 10/8/20.
+//  Copyright © 2020 com.velvetwire. All rights reserved.
+//
+
+#import "AssetDevice.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UpdateAsset : AssetDevice
+
+@end
+
+NS_ASSUME_NONNULL_END
